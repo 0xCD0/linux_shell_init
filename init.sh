d16819877d263447ef521e46d6f6cb4c1147cc3a
0xCD <64bitm@gmail.com>
2022-09-25 14:31:05 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,78 +1,78 @@
 #!/bin/bash
-echo Install Noto Sans CJK fonts
+echo ================== Install Noto Sans CJK fonts ==================
 yes | sudo pacman -Syu noto-fonts-cjk
 
-echo Install Ibus
+echo ================== Install Ibus ==================
 yes | sudo pacman -Syu ibus ibus-hangul ibus-anthy
 
-echo Install neofetch
+echo ================== Install neofetch ==================
 yes | sudo pacman -Syu neofetch
 
-echo Install zsh
+echo ================== Install zsh ==================
 yes | sudo pacman -Syu zsh
 
-echo Install OhMyZsh
+echo ================== Install OhMyZsh ==================
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo Install zsh-autosuggestions
+echo ================== Install zsh-autosuggestions ==================
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo Install zsh-syntax-highlighting
+echo ================== Install zsh-syntax-highlighting ==================
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo Change zsh Theme
+echo ================== Change zsh Theme ==================
 sed -i '0,/robbyrussell/s//fox/' ~/.zshrc
 
-echo Add plugins to zshrc
+echo ================== Add plugins to zshrc ==================
 sed -i '0,/plugins=(git)/s//plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
-echo Show neofetch when tty start
+echo ================== Show neofetch when tty start ==================
 echo neofetch >> ~/.zshrc
 
-echo Install Terminator
+echo ================== Install Terminator ==================
 yes | sudo pacman -Syu terminator
 
-echo Install VSCode
+echo ================== Install VSCode ==================
 yes | yay -Syu visual-studio-code-bin
 
-echo Install gedit
+echo ================== Install gedit ==================
 yes | sudo pacman -Syu gedit
 
-echo Install gnome disk utility
+echo ================== Install gnome disk utility ==================
 yes | sudo pacman -Syu gnome-disk-utility
 
-echo Install Spotify
+echo ================== Install Spotify ==================
 yes | yay -Syu spotify
 
-echo Install Google Chrome
+echo ================== Install Google Chrome ==================
 yes | yay -Syu google-chrome
 
 # if Plasma
 if [ $DESKTOP_SESSION = "plasma" ]
 then
-    echo Restore plasma settings
+    echo ================== Restore plasma settings ==================
     mv 0xcd_transfusion_20220925_1237.tar.gz zeroxcd_transfusion_20220925_1237.tar.gz
     chmod +x transfuse.sh
     echo 1 | transfuse.sh -r zeroxcd
 
-    echo Move ibus input method to autostart
+    echo ================== Move ibus input method to autostart ==================
     echo "export GTK_IM_MODULE=ibus\r\nexport QT_IM_MODULE=ibus\r\nexport XMODIFIERS=@im=ibus\r\nibus-daemon -drx" > ~/.config/autostart/input.sh
     echo "[Desktop Entry]\r\nExec=/home/$USERNAME/.config/autostart/input.sh\r\nIcon=\r\nName=input.sh\r\nPath=\r\nTerminal=False\r\nType=Application\r\n" > ~/.config/autostart/input.sh.desktop
 
     chmod +x ~/.config/autostart/input.sh
     chmod +x ~/.config/autostart/input.sh.desktop
 
-    echo Remove Konsole
+    echo ================== Remove Konsole ==================
     yes | sudo pacman -Rs konsole
 fi
 
 # if EndeavourOS
 if hostnamectl | grep -q EndeavourOS
 then
-    echo Install Nvidia driver install
+    echo ================== Install Nvidia driver install ==================
     nvidia-inst --legacyrepo
     nvidia-inst --series 470
 fi
 
-echo Set default zsh 
+echo ================== et default zsh ==================
 chsh -s /usr/bin/zsh
