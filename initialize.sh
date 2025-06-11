f446bec334421cc0ddba65c6f2ac0c2ddd7a0dbb
0xCD <64bitm@gmail.com>
2025-06-11 17:44:10 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,9 @@
 # Pacman
-yes | sudo pacman -Syu noto-fonts-cjk festfetch zsh git neovim kitty gnome-disk-utility cups curl fcitx5 fcitx5-hangul fcitx5-mozc imagemagick
+yes | sudo pacman -Syu noto-fonts-cjk festfetch zsh git neovim kitty gnome-disk-utility cups curl fcitx5 fcitx5-hangul fcitx5-mozc imagemagick bat
 
 # Yay
-yes | yay -Syu visual-studio-code-bin gedit gnome-disk-utility google-chrome getnf
+# getnf
+yes | yay -Syu visual-studio-code-bin gedit gnome-disk-utility google-chrome logo-ls
 
 # OhMyZsh
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -11,6 +12,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 sed -i '0,/robbyrussell/s//fox/' ~/.zshrc
 sed -i '0,/plugins=(git)/s//plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 chsh -s /usr/bin/zsh
+
+# powerlevel10k
+yes | yay -S --noconfirm zsh-theme-powerlevel10k-git && echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 
 if [ "$DESKTOP_SESSION" = "gnome" ]; then
